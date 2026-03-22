de906b0f03c0256d8334cacf69c801bbde4dc5fe
pavel <lana.pchelket@gmail.com>
2026-03-22 19:53:41 +0000

    add length of string

--- my_printf.s
+++ my_printf.s
@@ -6,7 +6,7 @@ _start:
 
                 
                 mov rdi, string
-               ;mov rcx, str_length
+                mov rcx, str_length
                 call my_printf
                 
                 mov rax, 0x3C      ; exit64 (rdi)
@@ -22,31 +22,30 @@ my_printf:
 
                 push rax
                 push rbx
+                push r8
 
 next_char:      
-                mov rax, '$'
-                cmp al, byte [rdi]  ; test end of string
-                je end_printf
-
                 mov rax, 0x01
                 lea rsi, [rel rdi]
                 mov rbx, rdi
                 mov rdi, 1
                 mov rdx, 1
+                mov r8, rcx
                 syscall
 
+                mov rcx, r8
                 mov rdi, rbx
                 inc rdi
-                ;loop next_char
-                jmp next_char
+                loop next_char
 
                 
 
+                pop r8
 end_printf:     pop rbx
                 pop rax
                 ret
 
 section .data
 
-string: db "hello world!$", 0xa
+string: db "hello world!", 0xa
 str_length equ $ - string
